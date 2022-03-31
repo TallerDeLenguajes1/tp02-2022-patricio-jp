bde3f7ac374a2ff6fc1ba6309fa6a4bafad3e459
Patricio Agustín Jimenez Paz <patojimenezpaz@gmail.com>
2022-03-31 17:59:23 -0300

    Terminado apartado a)

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,12 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #define N 20
 
-void main() {
+int main() {
+    srand(time(0));
     int i;
     double vt[N];
     
     for(i = 0; i<N; i++) {
-        vt[i]=1+rand()%100;
-        printf("%d", vt[i]);
+        vt[i] = 1 + rand() % 100;
+        printf("%f ", vt[i]);
     }
 
+    return 0;
 }
